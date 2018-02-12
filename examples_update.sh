6f6172ab60ea5760601f9ef4dbdd3f6f9b0d0d43
kenichi SHIMIZU <Pooh@Sputnik.local>
2018-02-13 00:05:55 +0900

    first step

---
+++ examples_update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+dst='../wifi-manager-dual-port-and-configure/examples/WiFiManager_Baena_Basic/'
+cp src/WiFiManager_Baena_Basic.cpp $dst/WiFiManager_Baena_Basic.ino
+cp src/my_wifi_manager.cpp $dst
+cp src/my_wifi_manager.h $dst
